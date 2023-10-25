195ce9e393ec19b9c85cefc55c1d98ca625b5d49
anabbaa <ahmed.nabaa.an@gmail.com>
2023-10-25 16:12:00 +0200

    down

--- src/components/HomeSub.js
+++ src/components/HomeSub.js
@@ -1,16 +1,30 @@
 import React from "react";
+import axios from "axios";
+import fileDownload from 'js-file-download'
+
+const url = "http://localhost:3000/CV.pdf"
+
+const handleDownload = (url, filename) => {
+  axios.get(url, {
+    responseType: 'blob',
+  })
+  .then((res) => {
+    fileDownload(res.data, filename)
+  })
+}
 
 const HomeSub =()=>{
-    return (
-      <React.Fragment>
+  return (
+<React.Fragment>
         <div className="home">
         <p >Hi</p>
         <p >I am Ahmed Nabbaa</p>
         <p >Welcome to my page</p>
         <p >enjoy</p>
-        <a className="cv" href="../../public/file/Ahmed Nabbaa CV.pdf" download>
-        My CV
-      </a>    
+      
+        <button className="cv" onClick={() => {handleDownload(url, 'CV.pdf')
+}}>Download MY CV</button>
+
         </div>
       </React.Fragment>
     )

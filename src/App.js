4bbfa7bd4fb89060184bf4c553b2852f238714e6
anabbaa <ahmed.nabaa.an@gmail.com>
2024-05-20 16:26:17 +0200

    router

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import Skills from "./components/Skills";
 import Badget from "./components/Badget";
 import Soon from "./components/Soon";
 import Loading from "./components/Loading";
-import {BrowserRouter as Router , Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import MobileNavbar from "./components/MobileNavbar";
 import DesktopNavbar from "./components/DesktopNavbar";
 
 
 function App() {
   const [load , setLoading] = useState(true);
-
-
 
   //this function for loading 
   useEffect(() => {
@@ -30,20 +29,19 @@ function App() {
      <Router>
      <DesktopNavbar />
     <MobileNavbar />
-    <Switch>   
-    <Route path="/myportffolioreact" exact component={Home}  />
-    <Route path="/" exact component={Home}  />
-    <Route path="/home" exact component={Home}  />    
-    <Route path="/aboutme" exact component={Aboutme} />
-    <Route
-        path="/projects" exact component={Projects}/>
-                <Route path="/skills" exact component={Skills} /> 
-        <Route path="/badget" exact component={Badget} /> 
-      <Route path="/contact" exact component={Contact} />
-      <Route path={() => "/main" || "/admin" || "/any-other-word"}>
-      <Soon />
-      </Route>  
-    </Switch>
+    <Routes >   
+    <Route path="/myportffolioreact" element={<Home/>} />
+    <Route path="/" element={<Home/>}  />
+    <Route path="/home" element={<Home/>}  />    
+    <Route  path="/aboutme" element={< Aboutme />} />
+    <Route path="/projects" element={<Projects />}/>
+    <Route path="/skills" element={<Skills />} /> 
+    <Route path="/badget" element={<Badget />} /> 
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/main" element={<Soon />} />
+        <Route path="/admin" element={<Soon />} />
+        <Route path="/any-other-word" element={<Soon />} />
+    </Routes>
     </Router>
   );       
 }

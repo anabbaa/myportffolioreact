92f3c35e30a165436c88f501ba890912c179d5a4
anabbaa <ahmed.nabaa.an@gmail.com>
2024-06-27 18:46:27 +0200

    sed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,49 +1,48 @@
-import React, {useState}  from "react";
+import React  from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 
 const Contact=({incFooter})=>{
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   message: ''
+  // });
 
+  // const { name, email, message } = formData;
 
-
-  const { name, email, message } = formData;
-
-  const onChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  // const onChange = (e) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
   
 
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await fetch('/api/messages', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-      console.log(response);
+  }
+    // try {
+    //   const response = await fetch('/api/messages', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(formData)
+    //   });
+    //   console.log(response);
 
-      const result = await response.json();
-      if (response.ok) {
-        alert(result.message);
-        setFormData({ name: '', email: '', message: '' });
-      } else {
-        alert('Error: ' + result.message);
-      }
-    } catch (err) {
-      console.error('Error submitting form:', err);
-      alert('Error submitting form');
-    }
-  };
+    //   const result = await response.json();
+    //   if (response.ok) {
+    //     alert(result.message);
+    //     setFormData({ name: '', email: '', message: '' });
+    //   } else {
+    //     alert('Error: ' + result.message);
+    //   }
+    // } catch (err) {
+    //   console.error('Error submitting form:', err);
+    //   alert('Error submitting form');
+    // }
+  
 
   return (
     <React.Fragment>
@@ -57,16 +56,12 @@ const Contact=({incFooter})=>{
       <input
         type="text"
         name="name"
-        onChange={onChange}
-        value={name}
         placeholder="Please Write Your Name"
         required
       />
       <input
         type="email"
         name="email"
-        onChange={onChange}
-        value={email}
         placeholder="Please Write Your Email"
         required
       />
@@ -74,8 +69,6 @@ const Contact=({incFooter})=>{
       <textarea
         name="message"
         placeholder="Please Write Your Message"
-        onChange={onChange}
-        value={message}
         required
       />
       <button className="send" type="submit">Send</button>
